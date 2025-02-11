78617b9e9209467378da09214ce3e3a4c19a303f
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-02-11 16:33:07 +0300

    feat(swagger): options for swaggerDoc have been added

--- src/KebabDelivery.API/Program.cs
+++ src/KebabDelivery.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Kebab API",
+        Version = "v1",
+        Description = "API для управления заказами и меню Kebab кафе"
+    });
+});
 
 var app = builder.Build();
 
@@ -15,6 +25,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
