ee9172ee2c7ce7fbebecb6d9edca5b4bf11d5512
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-02-13 17:12:16 +0300

    feat(ApplicationDbContext): DbContext has been added

--- src/KebabDelivery.Infrastructure/Data/ApplicationDbContext.cs
+++ src/KebabDelivery.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,5 +1,20 @@
-﻿namespace KebabDelivery.Infrastructure.Data;
+﻿using KebabDelivery.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
-public class ApplicationDbContext
+namespace KebabDelivery.Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext
 {
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<ProductSize> ProductSizes { get; set; }
+    public DbSet<Ingredient> Ingredients { get; set; }
+    public DbSet<ProductIngredient> ProductIngredients { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
 }
