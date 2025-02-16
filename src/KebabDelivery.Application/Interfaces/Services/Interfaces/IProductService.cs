2d618cb5c5ab04db5c15958f026ea29c34384061
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-02-16 11:03:20 +0300

    feat(servise): interface && service for product have been added.

---
+++ src/KebabDelivery.Application/Interfaces/Services/Interfaces/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using KebabDelivery.Application.DTOs;
+
+namespace KebabDelivery.Application.Interfaces.Services.Interfaces
+{
+    public interface IProductService
+    {
+        Task<ProductResponse?> GetByIdAsync(Guid id);
+        Task<List<ProductResponse>> GetAllAsync();
+        Task<ProductResponse> CreateAsync(ProductRequest request);
+        Task<ProductResponse> UpdateAsync(Guid id, ProductRequest request);
+        Task<bool> DeleteAsync(Guid id);
+    }
+}
---
+++ src/KebabDelivery.Application/Interfaces/Services/ProductService.cs
@@ -0,0 +1,64 @@
+﻿using KebabDelivery.Application.DTOs;
+using KebabDelivery.Application.Interfaces.Services.Interfaces;
+using KebabDelivery.Application.Mapping;
+using KebabDelivery.Domain.Entities;
+using KebabDelivery.Infrastructure.Data.Repositories.Interfaces;
+
+namespace KebabDelivery.Application.Interfaces.Services;
+
+public class ProductService : IProductService
+{
+    private readonly IProductRepository _productRepository;
+
+    public ProductService(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public async Task<ProductResponse> CreateAsync(ProductRequest request)
+    {
+        var result = Product.Create(request.Name, request.Description, request.ImageUrl, request.IsComposite);
+        if (result.IsFailed)
+            throw new Exception(result.Errors.First().Message);
+
+        var product = result.Value;
+        await _productRepository.AddAsync(product);
+        return product.ToResponse();
+    }
+
+    public async Task<bool> DeleteAsync(Guid id)
+    {
+        var product = await _productRepository.GetByIdAsync(id);
+        if (product is null) return false;
+
+        await _productRepository.DeleteAsync(id);
+        return true;
+    }
+
+    public async Task<List<ProductResponse>> GetAllAsync()
+    {
+        var products = await _productRepository.GetAllAsync();
+        return products.ToResponceList();
+    }
+
+    public async Task<ProductResponse?> GetByIdAsync(Guid id)
+    {
+        var product = await _productRepository.GetByIdAsync(id);
+        return product?.ToResponse();
+    }
+
+    public async Task<ProductResponse> UpdateAsync(Guid id, ProductRequest request)
+    {
+        var product = await _productRepository.GetByIdAsync(id);
+        if (product is null) return null;
+
+        var updatedProduct = Product.Create(request.Name, request.Description, request.ImageUrl, request.IsComposite);
+        if (updatedProduct.IsFailed)
+            throw new Exception(updatedProduct.Errors.First().Message);
+
+        product = updatedProduct.Value;
+        await _productRepository.UpdateAsync(product);
+
+        return product.ToResponse();
+    }
+}
