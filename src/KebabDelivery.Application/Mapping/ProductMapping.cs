ae85bd17093311ccf1b40fa0c6843dd4f97290e7
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-02-16 11:03:56 +0300

    feat(mapper): mapper for Product has been added.

---
+++ src/KebabDelivery.Application/Mapping/ProductMapping.cs
@@ -0,0 +1,24 @@
+﻿using KebabDelivery.Application.DTOs;
+using KebabDelivery.Domain.Entities;
+
+namespace KebabDelivery.Application.Mapping;
+
+public static class ProductMapping
+{
+    public static ProductResponse ToResponse(this Product product)
+    {
+        return new ProductResponse(
+            product.Id,
+            product.Name,
+            product.Description,
+            product.ImageUrl,
+            product.IsComposite,
+            product.ProductSizes,
+            product.ProductIngredients);
+    }
+
+    public static List<ProductResponse> ToResponceList(this List<Product> products)
+    {
+        return products.Select(p => p.ToResponse()).ToList();
+    }
+}
