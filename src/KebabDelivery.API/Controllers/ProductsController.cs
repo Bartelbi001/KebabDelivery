266f4dc45e5063f066428e0728385b76a40acff0
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-02-16 12:06:17 +0300

    feat(controller): ProductController has been added.

---
+++ src/KebabDelivery.API/Controllers/ProductsController.cs
@@ -0,0 +1,55 @@
+﻿using KebabDelivery.Application.DTOs;
+using KebabDelivery.Application.Interfaces.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KebabDelivery.API.Controllers;
+
+[ApiController]
+[Route("api/products")]
+public class ProductsController : ControllerBase
+{
+    private readonly IProductService _productService;
+
+    public ProductsController(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<ProductResponse>>> GetAll()
+    {
+        var products = await _productService.GetAllAsync();
+        return Ok(products);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<ProductResponse>> GetById(Guid id)
+    {
+        var product = await _productService.GetByIdAsync(id);
+        if (product is null) return NotFound();
+        return Ok(product);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ProductResponse>> Create(ProductRequest request)
+    {
+        var product = await _productService.CreateAsync(request);
+        return CreatedAtAction(nameof(GetById), new { id = product.Id }, product);
+    }
+
+    [HttpPut("{id:guid}")]
+    public async Task<IActionResult> Update(Guid id, ProductRequest request)
+    {
+        var updatedProduct = await _productService.UpdateAsync(id, request);
+        if (updatedProduct is null) return NotFound();
+        return NoContent();
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        var success = await _productService.DeleteAsync(id);
+        if (!success) return NotFound();
+        return NoContent();
+    }
+}
