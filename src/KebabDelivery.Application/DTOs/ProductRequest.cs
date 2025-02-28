357ab8f2ddb83d6257ff4e28e302ccd8f85c22c8
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-02-28 17:11:54 +0300

    feat(constructor): public constructor for JSON deserialization has been added.

--- src/KebabDelivery.Application/DTOs/ProductRequest.cs
+++ src/KebabDelivery.Application/DTOs/ProductRequest.cs
@@ -1,3 +1,23 @@
 ﻿namespace KebabDelivery.Application.DTOs;
 
-public record class ProductRequest(string Name, string Description, string ImageUrl, bool IsComposite, bool IsVisible);
+public record class ProductRequest
+{
+    public string Name { get; init; } = string.Empty;
+    public string Description { get; init; } = string.Empty;
+    public string ImageUrl { get; init; } = string.Empty;
+    public bool IsComposite { get; init; }
+    public bool IsVisible { get; init; }
+
+    // Пустой конструктор (нужен для JSON-десериализации)
+    public ProductRequest() { }
+
+    // Основной конструктор (чтобы вручную создавать объект в коде)
+    public ProductRequest(string name, string description, string imageUrl, bool isComposite, bool isVisible)
+    {
+        Name = name;
+        Description = description;
+        ImageUrl = imageUrl;
+        IsComposite = isComposite;
+        IsVisible = isVisible;
+    }
+}
