2d40770fa2f92b49f2b812bd3df89cf136643c83
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-03-05 21:52:29 +0300

    feat(middleware): middleware ExceptionHandlingMiddleware has been added.

---
+++ src/KebabDelivery.API/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,89 @@
+﻿using Serilog;
+using System.Net;
+using System.Text.Json;
+using ValidationException = FluentValidation.ValidationException;
+
+namespace KebabDelivery.API.Middlewares;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (ValidationException ex)
+        {
+            await HandleValidationExceptionAsync(context, ex);
+        }
+        catch (UnauthorizedAccessException ex)
+        {
+            await HandleExceptionAsync(context, ex, HttpStatusCode.Unauthorized, "Ошибка авторизации");
+        }
+        catch (KeyNotFoundException ex)
+        {
+            await HandleExceptionAsync(context, ex, HttpStatusCode.NotFound, "Ресурс не найден");
+        }
+        catch (InvalidOperationException ex)
+        {
+            await HandleExceptionAsync(context, ex, HttpStatusCode.Conflict, "Конфликт данных");
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(context, ex, HttpStatusCode.InternalServerError, "Внутренняя ошибка сервера");
+        }
+    }
+
+    private Task HandleValidationExceptionAsync(HttpContext context, ValidationException ex)
+    {
+        context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+        context.Response.ContentType = "application/json";
+
+        var errors = ex.Errors.Select(e => new
+        {
+            Field = e.PropertyName,
+            Error = e.ErrorMessage
+        });
+
+        var response = new
+        {
+            Message = "Ошибка валидации",
+            Errors = errors
+        };
+
+        Log.Warning("Ошибка валидации: {@Errors}", errors);
+
+        return context.Response.WriteAsync(JsonSerializer.Serialize(response));
+    }
+
+    private Task HandleExceptionAsync(HttpContext context, Exception ex, HttpStatusCode statusCode, string message)
+    {
+        context.Response.StatusCode = (int)statusCode;
+        context.Response.ContentType = "application/json";
+
+        var response = new
+        {
+            Message = message,
+            Details = ex.Message
+        };
+
+        if (statusCode == HttpStatusCode.InternalServerError)
+        {
+            Log.Fatal(ex, "Фатальная ошибка: {Message}", ex.Message);
+        }
+        else
+        {
+            Log.Error(ex, "Произошла ошибка: {Message}", ex.Message);
+        }
+
+        return context.Response.WriteAsync(JsonSerializer.Serialize(response));
+    }
+}
