6ef018a347b4c7882fe1c8b799edec89327fd23e
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-03-05 21:51:53 +0300

    feat(validation): ProductValidator has been added.

---
+++ src/KebabDelivery.Application/Validators/ProductValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using KebabDelivery.Domain.Entities;
+
+namespace KebabDelivery.Application.Validators;
+
+public class ProductValidator : AbstractValidator<Product>
+{
+    public ProductValidator()
+    {
+        RuleFor(p => p.Name)
+            .NotEmpty().WithMessage("Поле 'Name' не может быть пустым.")
+            .MaximumLength(100).WithMessage("Поле 'Name' не должно превышать 100 символов.");
+
+        RuleFor(p => p.Description)
+            .NotEmpty().WithMessage("Поле 'Description' не может быть пустым.")
+            .MaximumLength(500).WithMessage("Поле 'Description' не должно превышать 500 символов.");
+
+        RuleFor(p => p.ImageUrl)
+            .NotEmpty().WithMessage("Поле 'ImageUrl' не может быть пустым.")
+            .Must(uri => Uri.TryCreate(uri, UriKind.Absolute, out _))
+            .WithMessage("Поле 'ImageUrl' должно содержать корректный URL.");
+    }
+}
