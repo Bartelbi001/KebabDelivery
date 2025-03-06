64c253b126612932ebd71b01145a9270851bd3a8
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-03-06 18:22:49 +0300

    feat(class): the validator have created.

---
+++ src/KebabDelivery.Application/Validators/IngredientValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using KebabDelivery.Domain.Entities;
+
+namespace KebabDelivery.Application.Validators;
+
+public class IngredientValidator : AbstractValidator<Ingredient>
+{
+    public IngredientValidator()
+    {
+        RuleFor(i => i.Name)
+            .NotEmpty().WithMessage("Поле 'Name' не может быть пустым.")
+            .MaximumLength(100).WithMessage("Поле 'Name' не должно превышать 100 символов.");
+
+        RuleFor(i => i.Calories)
+            .GreaterThanOrEqualTo(0).WithMessage("Калории не могут быть отрицательными");
+
+        RuleFor(i => i.Proteins)
+            .GreaterThanOrEqualTo(0).WithMessage("Белки не могут быть отрицательными");
+
+        RuleFor(i => i.Fats)
+            .GreaterThanOrEqualTo(0).WithMessage("Жиры не могут быть отрицательными");
+
+        RuleFor(i => i.Carbohydrates)
+            .GreaterThanOrEqualTo(0).WithMessage("Углеводы не могут быть отрицательными");
+    }
+}
