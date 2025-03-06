c9eb39aeb367ddf8a6d3680b58b835908b5e9e98
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-03-06 18:22:10 +0300

    feat(dto): the DTO's have created.

---
+++ src/KebabDelivery.Application/DTOs/IngredientRequest.cs
@@ -0,0 +1,25 @@
+﻿namespace KebabDelivery.Application.DTOs;
+
+public class IngredientRequest
+{
+    public string Name { get; set; } = string.Empty;
+    public decimal Calories { get; set; }
+    public decimal Proteins { get; set; }
+    public decimal Fats { get; set; }
+    public decimal Carbohydrates { get; set; }
+    public bool? IsAlcoholic { get; set; } // NULL = Не применимо
+    public bool? ContainsLactose { get; set; } // NULL = Не применимо
+
+    public IngredientRequest() { }
+
+    public IngredientRequest(string name, decimal calories, decimal proteins, decimal fats, decimal carbs, bool isAlcoholic, bool containsLactose)
+    {
+        Name = name;
+        Calories = calories;
+        Proteins = proteins;
+        Fats = fats;
+        Carbohydrates = carbs;
+        IsAlcoholic = isAlcoholic;
+        ContainsLactose = containsLactose;
+    }
+}
---
+++ src/KebabDelivery.Application/DTOs/IngredientResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace KebabDelivery.Application.DTOs;
+
+public record IngredientResponse(
+    Guid Id,
+    string Name,
+    decimal Calories,
+    decimal Proteins,
+    decimal Fats,
+    decimal Carbohydrates,
+    bool? IsAlcoholic, // NULL = Не применимо
+    bool? ContainsLactose); // NULL = Не применимо
