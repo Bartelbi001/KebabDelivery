1278ee0de5b042fae46f2b7549ad7677cd03d06a
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-03-06 18:22:36 +0300

    feat(class): the mapping have created.

---
+++ src/KebabDelivery.Application/Mapping/IngredientMapping.cs
@@ -0,0 +1,25 @@
+﻿using KebabDelivery.Application.DTOs;
+using KebabDelivery.Domain.Entities;
+
+namespace KebabDelivery.Application.Mapping;
+
+public static class IngredientMapping
+{
+    public static IngredientResponse ToResponse(this Ingredient ingredient)
+    {
+        return new IngredientResponse(
+            ingredient.Id,
+            ingredient.Name,
+            ingredient.Calories,
+            ingredient.Proteins,
+            ingredient.Fats,
+            ingredient.Carbohydrates,
+            ingredient.IsAlcoholic,
+            ingredient.ContainsLactose);
+    }
+
+    public static List<IngredientResponse> ToResponseList(this IEnumerable<Ingredient> ingredients)
+    {
+        return ingredients.Select(i => i.ToResponse()).ToList();
+    }
+}
