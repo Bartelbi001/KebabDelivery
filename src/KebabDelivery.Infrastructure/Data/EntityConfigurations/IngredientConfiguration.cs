124d4ba1b77d0ed4748cdfa462f63e32d7e3ecc6
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-03-06 18:23:07 +0300

    feat(class): the configuration have created.

--- src/KebabDelivery.Infrastructure/Data/EntityConfigurations/IngredientConfiguration.cs
+++ src/KebabDelivery.Infrastructure/Data/EntityConfigurations/IngredientConfiguration.cs
@@ -10,8 +10,33 @@ class IngredientConfiguration : IEntityTypeConfiguration<Ingredient>
     {
         builder.HasKey(i => i.Id);
 
+        builder.HasIndex(i => i.Name)
+            .IsUnique();
+
         builder.Property(i => i.Name)
             .IsRequired()
             .HasMaxLength(100);
+
+        builder.Property(i => i.Calories)
+            .IsRequired()
+            .HasColumnType("decimal(10,2)");
+
+        builder.Property(i => i.Proteins)
+            .IsRequired()
+            .HasColumnType("decimal(10,2)");
+
+        builder.Property(i => i.Fats)
+            .IsRequired()
+            .HasColumnType("decimal(10,2)");
+
+        builder.Property(i => i.Carbohydrates)
+            .IsRequired()
+            .HasColumnType("decimal(10,2)");
+
+        builder.Property(i => i.IsAlcoholic)
+            .HasDefaultValue(false);
+
+        builder.Property(i => i.ContainsLactose)
+            .HasDefaultValue(false);
     }
 }
