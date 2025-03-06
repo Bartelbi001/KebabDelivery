7c67e00fc3cf223e71e0bfa1f42e15285b930601
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-03-06 18:20:11 +0300

    feat(class): the interface was created and implemented in the repository.

---
+++ src/KebabDelivery.Infrastructure/Data/Repositories/IngredientRepository.cs
@@ -0,0 +1,50 @@
+﻿using KebabDelivery.Domain.Entities;
+using KebabDelivery.Infrastructure.Data.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace KebabDelivery.Infrastructure.Data.Repositories;
+
+class IngredientRepository : IIngredientRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public IngredientRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(Ingredient ingredient)
+    {
+        await _context.Ingredients.AddAsync(ingredient);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        var ingredient = await _context.Ingredients.FindAsync(id);
+        if (ingredient is null) return;
+
+        _context.Remove(ingredient);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<List<Ingredient>> GetAllAsync()
+    {
+        return await _context.Ingredients
+            .Include(i => i.ProductIngredients)
+            .ToListAsync();
+    }
+
+    public async Task<Ingredient?> GetByIdAsync(Guid id)
+    {
+        return await _context.Ingredients
+            .Include(i => i.ProductIngredients)
+            .FirstOrDefaultAsync(i => i.Id == id);
+    }
+
+    public async Task UpdateAsync(Ingredient ingredient)
+    {
+        _context.Ingredients.Update(ingredient);
+        await _context.SaveChangesAsync();
+    }
+}
---
+++ src/KebabDelivery.Infrastructure/Data/Repositories/Interfaces/IIngredientRepository.cs
@@ -0,0 +1,12 @@
+﻿using KebabDelivery.Domain.Entities;
+
+namespace KebabDelivery.Infrastructure.Data.Repositories.Interfaces;
+
+interface IIngredientRepository
+{
+    Task<Ingredient?> GetByIdAsync(Guid id);
+    Task<List<Ingredient>> GetAllAsync();
+    Task AddAsync(Ingredient ingredient);
+    Task DeleteAsync(Guid id);
+    Task UpdateAsync(Ingredient ingredient);
+}
