c544ebc2c847a207f4d44e73991a4145f8960340
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-03-07 19:22:04 +0300

    feat(controller): The IngredientController has been added.

---
+++ src/KebabDelivery.API/Controllers/IngredientsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KebabDelivery.API.Controllers;
+
+[ApiController]
+[Route("api/ingredients")]
+public class IngredientsController : ControllerBase
+{
+    public IngredientsController(IIngredientService ingredientService)
+    {
+
+    }
+}
