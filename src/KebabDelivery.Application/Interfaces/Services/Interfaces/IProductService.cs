0ae4bcd9c0a782fb5f5d0de2578b52b669d69120
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-03-07 19:24:47 +0300

    refactor(class): " ; " has been added.

--- src/KebabDelivery.Application/Interfaces/Services/Interfaces/IProductService.cs
+++ src/KebabDelivery.Application/Interfaces/Services/Interfaces/IProductService.cs
@@ -1,13 +1,12 @@
 ﻿using KebabDelivery.Application.DTOs;
 
-namespace KebabDelivery.Application.Interfaces.Services.Interfaces
+namespace KebabDelivery.Application.Interfaces.Services.Interfaces;
+
+public interface IProductService
 {
-    public interface IProductService
-    {
-        Task<ProductResponse?> GetByIdAsync(Guid id);
-        Task<List<ProductResponse>> GetAllAsync();
-        Task<ProductResponse> CreateAsync(ProductRequest request);
-        Task<ProductResponse> UpdateAsync(Guid id, ProductRequest request);
-        Task<bool> DeleteAsync(Guid id);
-    }
+    Task<ProductResponse?> GetByIdAsync(Guid id);
+    Task<List<ProductResponse>> GetAllAsync();
+    Task<ProductResponse> CreateAsync(ProductRequest request);
+    Task<ProductResponse> UpdateAsync(Guid id, ProductRequest request);
+    Task<bool> DeleteAsync(Guid id);
 }
