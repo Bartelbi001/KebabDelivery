83f5f3186844d2d47af16133736e37017e9b6d6f
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-03-08 03:25:20 +0300

    feat(class): a new class IngredientIngredient has been added.

---
+++ src/KebabDelivery.Domain/Entities/IngredientIngredient.cs
@@ -0,0 +1,34 @@
+﻿using KebabDelivery.Domain.Enums;
+using KebabDelivery.Domain.Exceptions;
+
+namespace KebabDelivery.Domain.Entities;
+
+public class IngredientIngredient
+{
+    public Guid IngredientId { get; private set; }
+    public Ingredient Ingredient { get; private set; }
+    public Guid SubIngredientId { get; private set; }
+    public Ingredient SubIngredient { get; private set; }
+    public decimal Amount { get; private set; }
+    public MeasurementUnit Unit { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+
+    private IngredientIngredient() { }
+
+    public IngredientIngredient(Ingredient ingredient, Ingredient subIngredient, decimal amount, MeasurementUnit unit)
+    {
+        if (ingredient == null)
+            throw new DomainValidationException("Главный ингредиент не может быть null.");
+
+        if (subIngredient == null)
+            throw new DomainValidationException("Подингредиент не может быть null.");
+
+        Ingredient = ingredient;
+        IngredientId = ingredient.Id;
+        SubIngredient = subIngredient;
+        SubIngredientId = subIngredient.Id;
+        Amount = amount;
+        Unit = unit;
+        CreatedAt = DateTime.UtcNow;
+    }
+}
