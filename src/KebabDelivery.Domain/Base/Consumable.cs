ee49ecd06585c4f955d8ddd059d1faa0c8ae57f6
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-03-08 03:24:44 +0300

    feat(class): base class for Ingredient && Product has been added.

---
+++ src/KebabDelivery.Domain/Base/Consumable.cs
@@ -0,0 +1,33 @@
+﻿using KebabDelivery.Domain.Exceptions;
+
+namespace KebabDelivery.Domain.Base;
+
+public abstract class Consumable
+{
+    public Guid Id { get; private set; }
+    public string Name { get; private set; }
+    public bool IsAlcoholic { get; protected set; }
+    public bool ContainsLactose { get; protected set; }
+    public decimal Calories { get; protected set; }
+    public decimal Proteins { get; protected set; }
+    public decimal Fats { get; protected set; }
+    public decimal Carbohydrates { get; protected set; }
+
+    protected Consumable(string name, bool isAlcoholic, bool containsLactose, decimal calories, decimal proteins, decimal fats, decimal carbohydrates)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new DomainValidationException("Имя не может быть пустым.");
+
+        if (calories < 0 || proteins < 0 || fats < 0 || carbohydrates < 0)
+            throw new DomainValidationException("Значения БЖУ не могут быть отрицательными.");
+
+        Id = Guid.NewGuid();
+        Name = name;
+        IsAlcoholic = isAlcoholic;
+        ContainsLactose = containsLactose;
+        Calories = calories;
+        Proteins = proteins;
+        Fats = fats;
+        Carbohydrates = carbohydrates;
+    }
+}
