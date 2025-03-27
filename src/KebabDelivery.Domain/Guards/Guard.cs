deffb3034978ccde0fbb5c4f040243a5490b9870
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-03-27 20:47:32 +0300

    feat(class): The Guard.cs has been added.

---
+++ src/KebabDelivery.Domain/Guards/Guard.cs
@@ -0,0 +1,43 @@
+using KebabDelivery.Domain.Exceptions;
+
+namespace KebabDelivery.Domain.Guards;
+
+public static class Guard
+{
+    public static void AgainstNull<T>(T? value, string message)
+    {
+        if (value == null)
+            throw new DomainValidationException(message);
+    }
+
+    public static void AgainstNullOrWhiteSpace(string? value, string message)
+    {
+        if(string.IsNullOrWhiteSpace(value))
+            throw new DomainValidationException(message);
+    }
+
+    public static void AgainstNegative(decimal value, string message)
+    {
+        if(value < 0)
+            throw new DomainValidationException(message);
+    }
+
+    public static void AgainstNonPositive(decimal value, string message)
+    {
+        if (value <= 0)
+            throw new DomainValidationException(message);
+    }
+
+    public static void AgainstEqual<T>(T? value, T? other, string message)
+    {
+        if(EqualityComparer<T>.Default.Equals(value, other))
+            throw new DomainValidationException(message);
+    }
+
+    public static void AgainstEnumOutOfRange<TEnum>(TEnum value, string message)
+        where TEnum : struct, Enum
+    {
+        if (!Enum.IsDefined(typeof(TEnum), value))
+            throw new DomainValidationException(message);
+    }
+}
