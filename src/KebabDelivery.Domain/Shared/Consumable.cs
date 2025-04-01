9e16b854f02db4869ebeff77a005346edb8b61de
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-04-01 23:00:51 +0300

    chore(class): The Consumable.cs has been moved from Domain\Base to Domain\Shared and modified.

---
+++ src/KebabDelivery.Domain/Shared/Consumable.cs
@@ -0,0 +1,35 @@
+﻿using KebabDelivery.Domain.Common;
+using KebabDelivery.Domain.Exceptions;
+using KebabDelivery.Domain.Guards;
+using KebabDelivery.Domain.ValueObjects;
+
+namespace KebabDelivery.Domain.Base;
+
+public abstract class Consumable : EntityBase<Guid>
+{
+    public string Name { get; private set; }
+    public bool IsAlcoholic { get; protected set; }
+    public bool ContainsLactose { get; protected set; }
+    public Nutrition Nutrition { get; protected set; }
+    
+    protected Consumable() { }
+
+    protected Consumable(string name, bool isAlcoholic, bool containsLactose, Nutrition nutrition)
+        : base(Guid.NewGuid())
+    {
+        Guard.AgainstNullOrWhiteSpace(name, "Name is required.");
+        Guard.AgainstNull(nutrition, "Nutrition is required.");
+        
+        Name = name;
+        IsAlcoholic = isAlcoholic;
+        ContainsLactose = containsLactose;
+        Nutrition = nutrition;
+    }
+
+    protected void UpdateNutrition(Nutrition newNutrition)
+    {
+        Guard.AgainstNull(newNutrition, "Nutrition is required.");
+        Nutrition = newNutrition;
+        SetUpdatedNow();
+    }
+}
