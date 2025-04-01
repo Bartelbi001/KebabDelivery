cd941ba95ac62d4eb2eec68afb53385553131c6c
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-04-01 21:13:51 +0300

    feat(class): The ValueObject.cs has been created and implemented.

---
+++ src/KebabDelivery.Domain/Common/ValueObject.cs
@@ -0,0 +1,37 @@
+namespace KebabDelivery.Domain.Common;
+
+public abstract class ValueObject
+{
+    protected abstract IEnumerable<object?> GetEqualityComponents();
+
+    public override bool Equals(object? obj)
+    {
+        if(obj == null || obj.GetType() != GetType())
+            return false;
+
+        return GetEqualityComponents()
+            .SequenceEqual(((ValueObject)obj).GetEqualityComponents());
+    }
+
+    public override int GetHashCode()
+    {
+        return GetEqualityComponents()
+            .Aggregate(1, (current, obj) =>
+            {
+                unchecked
+                {
+                    return current * 23 + (obj?.GetHashCode() ?? 0);
+                }
+            });
+    }
+
+    public static bool operator ==(ValueObject? left, ValueObject? right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(ValueObject? left, ValueObject? right)
+    {
+        return !Equals(left, right);
+    }
+}
