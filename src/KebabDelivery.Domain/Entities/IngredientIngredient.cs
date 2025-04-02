6b9f85b787f62a2412eaf7f7f9a8e5b4a897073d
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-04-02 18:24:30 +0300

    chore(class): The IngredientIngredient.cs has been modified.

--- src/KebabDelivery.Domain/Entities/IngredientIngredient.cs
+++ src/KebabDelivery.Domain/Entities/IngredientIngredient.cs
@@ -1,6 +1,5 @@
-﻿using KebabDelivery.Domain.Enums;
-using KebabDelivery.Domain.Exceptions;
-using KebabDelivery.Domain.Guards;
+﻿using KebabDelivery.Domain.Guards;
+using KebabDelivery.Domain.ValueObjects;
 
 namespace KebabDelivery.Domain.Entities;
 
@@ -12,27 +11,24 @@ public class IngredientIngredient
     public Guid SubIngredientId { get; private set; }
     public Ingredient SubIngredient { get; private set; } = null!;
     
-    public decimal Amount { get; private set; }
-    public MeasurementUnit Unit { get; private set; }
+    
+    public Measurement Measurement { get; private set; } = null!;
     public DateTime CreatedAt { get; private set; }
 
-    protected IngredientIngredient() { }
+    private IngredientIngredient() { }
 
-    public IngredientIngredient(Ingredient ingredient, Ingredient subIngredient, decimal amount, MeasurementUnit unit)
+    public IngredientIngredient(Ingredient ingredient, Ingredient subIngredient, Measurement measurement)
     {
-        Guard.AgainstNull(ingredient, "Main ingredient cannot be null.");
-        Guard.AgainstNull(subIngredient, "Sub ingredient cannot be null.");
-        Guard.AgainstEqual(ingredient.Id, subIngredient.Id, "An ingredient cannot be under itself.");
+        Guard.AgainstNull(ingredient, "Ingredient is required.");
+        Guard.AgainstNull(subIngredient, "SubIngredient is required.");
+        Guard.AgainstNull(measurement, "Measurement is required.");
+        Guard.AgainstEqual(ingredient.Id, subIngredient.Id, "An ingredient cannot contain itself.");
         
-        if (amount <= 0)
-            throw new DomainValidationException("The number must be greater than 0.");
-
         Ingredient = ingredient;
         IngredientId = ingredient.Id;
-        SubIngredient = subIngredient;
         SubIngredientId = subIngredient.Id;
-        Amount = amount;
-        Unit = unit;
-        CreatedAt = DateTime.UtcNow;
+        SubIngredientId = subIngredient.Id;
+        Measurement = measurement;
+        CreatedAt = DateTime.Now;
     }
 }
